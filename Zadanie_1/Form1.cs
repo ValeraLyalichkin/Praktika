49b4551bde86c6fb40d74600dbeeb1862b9f60fc
ValeraLyalichkin <valeralialichkin2002@mai.ru>
2021-12-02 10:18:53 +0300

    Добавил пункт меню верхнего уровня File с командами Open и Exit

--- Zadanie_1/Form1.cs
+++ Zadanie_1/Form1.cs
@@ -16,5 +16,22 @@ namespace Zadanie_1
         {
             InitializeComponent();
         }
+
+        private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.Filter = "Файлы pdf|*.pdf";
+            openFileDialog1.ShowDialog();
+            axAcroPDF1.LoadFile(openFileDialog1.FileName);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
     }
 }
